2891deffbbf45ff2908c2ce83b163ecd149ac7cf
Lucas Santana <lucasadd15@gmail.com>
2023-02-16 10:56:18 -0300

    teste

--- index.php
+++ index.php
@@ -2,22 +2,18 @@
 <html>
 <head>
 	<title>Formulário</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 	<h1>Formulário</h1>
-	<form method="post" action="enviar.php">
+	<form class="meu-formulario" method="post" action="enviar.php">
 		<label for="texto">Digite um texto:</label>
-		<input type="text" id="texto" name="texto" required>
-		<br><br>
-        <label for="numero">Digite um número:</label>
-		<input type="number" id="numero" name="numero" required>
-        <br><br>
+		<input type="text" id="texto" name="texto" class="meu-input" required>
 		<label for="imagem">Insira uma URL de imagem:</label>
-		<input type="text" id="imagem" name="imagem" required>
-        
-		<br><br>
-		<br><br>
-		<input type="submit" value="Enviar">
+		<input type="text" id="imagem" name="imagem" class="meu-input" required>
+		<label for="numero">Digite um número:</label>
+		<input type="number" id="numero" name="numero" class="meu-input" required>
+		<input type="submit" value="Enviar" class="meu-botao">
 	</form>
 </body>
 </html>
