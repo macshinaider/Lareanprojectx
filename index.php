3a246adbf34d7c0f87825847b97267a592198426
Lucas Santana <lucasadd15@gmail.com>
2023-02-16 10:48:24 -0300

    espaço entre o numero e a url

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 		<br><br>
 		<label for="imagem">Insira uma URL de imagem:</label>
 		<input type="text" id="imagem" name="imagem" required>
+        <br><br>
         <label for="numero">Digite um número:</label>
 		<input type="number" id="numero" name="numero" required>
 		<br><br>
